594dbbb9a07d4f9956cc21620a9c3b44baa9c66c
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-05-13 06:01:32 +0000

    Create Working with Files.py

---
+++ Python Village/Working with Files.py
@@ -0,0 +1,22 @@
+'''
+
+Problem
+  Given: A file containing at most 1000 lines.
+  Return: A file containing all the even-numbered lines from the original file.  Assume 1-based numbering of lines.
+'''
+i = 1
+
+readFile = open('rosalind_ini5.txt', 'r')   
+outFile = open('output.txt', 'w')      
+for line in readFile.readlines():
+      
+      
+        	if i % 2 == 0 :
+      
+      
+        
+		outFile.write(line)
+      
+      
+        
+	i+= 1
