ac5514dffb1d0a3d03909e93fdcfb6d2677cb17a
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-05-13 06:02:48 +0000

    Update Working with Files.py

--- Python Village/Working with Files.py
+++ Python Village/Working with Files.py
@@ -9,14 +9,6 @@ i = 1
 readFile = open('rosalind_ini5.txt', 'r')   
 outFile = open('output.txt', 'w')      
 for line in readFile.readlines():
-      
-      
-        	if i % 2 == 0 :
-      
-      
-        
+	if i % 2 == 0 :
 		outFile.write(line)
-      
-      
-        
 	i+= 1
