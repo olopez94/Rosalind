94eb86946ca71ba07c31391d77f762b683be72d8
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-05-13 03:14:38 +0000

    Create Variables and Some Arithmetic.py

---
+++ Python Village/Variables and Some Arithmetic.py
@@ -0,0 +1,10 @@
+'''
+Problem
+  Given: Two positive integers a and b, each less than 1000.
+  Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+'''
+
+a = 123
+b = 456
+c = a**2 + b**2
+print(c)
