b331b786f2354687b20f61d3d679c5c80353b396
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-05-16 02:28:04 +0000

    Create Translating RNA into Protein.py

---
+++ Bioinformatics Stronghold/Translating RNA into Protein.py
@@ -0,0 +1,38 @@
+'''
+
+codonTable = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'STOP', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'STOP', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'STOP', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G' 
+}
+'''
+
+
+
+dnaString = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+
+protein = ''
+i = 0
+while i < len(dnaString) - 3:
+    
+    code = dnaString[i:i+3]
+    if code != 'UAA' or code != 'UAG' or code != 'UGA0':
+        protein += codonTable[code]
+    else:
+        break
+    i += 3
+
+print(protein)
