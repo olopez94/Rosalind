e1c6ab448a338ccb71eef708239531b6d7abaac7
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-08-09 15:20:24 -0500

    Update 03_GBK.py

--- Bioinformatics Armory/GenBank Introduction/03_GBK.py
+++ Bioinformatics Armory/GenBank Introduction/03_GBK.py
@@ -4,7 +4,7 @@ with open('dataset/rosalind_gbk.txt') as input_data:
     genus, start_date, end_date = [line.strip() for line in input_data.readlines()]
 
 
-Entrez.email="oscar.lopez94@protonmail.com"
+Entrez.email="***@mail.com"
 
 handle= Entrez.esearch(db="nucleotide", term=genus+"[ORGN]", mindate=start_date, maxdate=end_date, datetype="pdat")
 
